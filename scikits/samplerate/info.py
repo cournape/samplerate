1954214b0369ff135185882dd89ccbeaeffc125d
David Cournapeau <cournape@gmail.com>
2008-01-18 15:56:04 +0000

    Update license, and name change

--- scikits/samplerate/info.py
+++ scikits/samplerate/info.py
@@ -1,13 +1,13 @@
-""" pysamplerate is a small python package to resample audio data in numpy arrays
-to a difference sampling rate: it is basically a wrapper around the Secret Rabbit Code
-from Erik Castro De Lopo (http://www.mega-nerd.com/SRC/). This package only makes sense
-for audio data, and has high quality converters based on the work of J.O Smith from
-CCRMA (see http://ccrma.stanford.edu/~jos/resample/optfir.pdf)
+"""samplerate is a small python package to resample audio data in numpy arrays
+to a difference sampling rate: it is basically a wrapper around the Secret
+Rabbit Code from Erik Castro De Lopo (http://www.mega-nerd.com/SRC/).  This
+package only makes sense for audio data, and has high quality converters based
+on the work of J.O Smith from CCRMA (see
+http://ccrma.stanford.edu/~jos/resample/optfir.pdf)
     
 2006, David Cournapeau
 
-LICENSE: the license of pysamplerate is the same than scipy, but beware that the license
-of libsndfile itself is GPL."""
+LICENSE: the license of samplerate is GPL, as SRC itself."""
 
 # version of the python module
 version                 = '0.1'
