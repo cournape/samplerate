21acd00c66157a990963089986ea4fef47b8b208
David Cournapeau <cournape@gmail.com>
2008-09-26 11:01:36 +0000

    Add example.

---
+++ scikits/samplerate/doc/src/examples/example1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pylab as plt
+from scikits.resample import converter_format, resample
+
+fs  = 44100.
+fr  = 48000.
+# Signal to resample
+sins    = np.sin(2 * np.pi * 1000/fs * np.arange(0, fs * 2))
+# Ideal resampled signal
+idsin   = np.sin(2 * np.pi * 1000/fr * np.arange(0, fr * 2))
+
+conv1   = resample(sins, fr/fs, converter_format('linear'))
+conv3   = resample(sins, fr/fs, converter_format('sinc_best'))
+
+err1    = conv1[fr:fr+2000].T - idsin[fr:fr+2000]
+err3    = conv3[fr:fr+2000].T - idsin[fr:fr+2000]
+
+plt.subplot(3, 1, 1)
+plt.plot(idsin[fs:fs+2000])
+plt.title('Resampler residual quality comparison')
+
+plt.subplot(3, 1, 2)
+plt.plot(err1)
+plt.ylabel('Linear')
+
+plt.subplot(3, 1, 3)
+plt.plot(err3)
+plt.ylabel('Sinc')
+
+plt.savefig('example1.png', dpi = 100)
