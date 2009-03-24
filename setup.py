b5ca55be81ed46fe6bd22d71d9dfb226a04947b3
David Cournapeau <cournape@gmail.com>
2009-03-24 15:08:05 +0000

    Generate the version py file for docs.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# Last Change: Tue Feb 17 03:00 PM 2009 J
+# Last Change: Tue Mar 24 11:00 PM 2009 J
 # TODO:
 #   - check how to handle cmd line build options with distutils and use
 #   it in the building process
@@ -30,6 +30,8 @@ def configuration(parent_package='',top_path=None, package_name=DISTNAME):
     pkg_prefix_dir = os.path.join('scikits', 'samplerate')
 
     write_version(os.path.join("scikits", "samplerate", "version.py"))
+    if os.path.exists(os.path.join("docs", "src")):
+        write_version(os.path.join("docs", "src", "samplerate_version.py"))
 
     from numpy.distutils.misc_util import Configuration
     config = Configuration(None,parent_package,top_path,
