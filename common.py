bb37b402a3c1338ad1c110a8f998dc376fa4048f
David Cournapeau <cournape@gmail.com>
2009-03-27 16:14:43 +0900

    Release 0.3.1.

--- common.py
+++ common.py
@@ -20,7 +20,7 @@ DOWNLOAD_URL        = 'http://pypi.python.org/pypi/scikits.samplerate'
 MAJOR = 0
 MINOR = 3
 MICRO = 1
-DEV = True
+DEV = False
 
 CLASSIFIERS = ['Development Status :: 4 - Beta',
         'Environment :: Console',
